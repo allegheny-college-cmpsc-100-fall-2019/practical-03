27d5db824707f0829d6d8d932f9d68e8a5171c23
Douglas Luman <dluman@douglasjluman.com>
2019-09-12 22:44:21 -0400

    Initial commit.

---
+++ src/main/java/puzzle/DisplayPuzzle.java
@@ -0,0 +1,18 @@
+    System.out.println(Pieces.tailAndLegs());
+   * @param args The command line arguments
+   *
+    // TODO Caption puzzle with a blank line followed by a caption of your choice!
+
+import puzzle.Pieces;
+  }
+/** This class displays the assembled puzzle.
+  public static void main(String[] args) {
+ */
+}
+    System.out.println(Pieces.hat());
+public class DisplayPuzzle {
+ * @author {YOUR NAME HERE}
+  /** The main entry point for DisplayPuzzle.
+   */
+ *
+    System.out.println(Pieces.body());
